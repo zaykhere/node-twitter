f45f2bbfae76d622f39ad6075dd2bef76a152af6
zain <zainjhere@gmail.com>
2024-05-05 19:43:12 +0500

    Displaying the posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const { requireLogin } = require("../middlewares/auth");
 const Post = require("../schemas/postSchema");
 
+router.get("/", requireLogin, async(req,res) => {
+  try {
+    const posts = await Post.find().populate('postedBy', '-password').sort({"createdAt": -1})
+    return res.status(200).json(posts)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error})
+  }
+})
+
 router.post("/", requireLogin, async(req,res) => {
-  console.log(req.body)
   if(!req.body.content) {
     return res.status(400).json({error: "You need to add some data"});
   }
